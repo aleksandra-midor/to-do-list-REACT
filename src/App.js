790b6905b32b277fcfec89f03c5240167b79d736
octane\ola <ola_midor@outlook.com>
2020-09-30 14:26:54 +0200

    displaying input values added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // Importing components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
+
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
-        <hi>Alex's to do list</hi>
+  <hi>Alex's to do list</hi>
       </header>
-      <Form />
-      <TodoList />
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText}/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
 import React from "react";
+// Import components
+import Todo from "./Todo";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+  console.log(todos)
   return (
     <div className="todo-container">
       <ul className="todo-list"></ul>
+      {todos.map(todo => (
+        <Todo text={todo.text} />
+      ))}
     </div>
   );
 };

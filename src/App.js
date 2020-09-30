8d199858cc3ac0eedf0bb7a3bc2a18a732ff477f
octane\ola <ola_midor@outlook.com>
2020-09-30 12:05:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// Importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <hi>Alex's to do list</hi>
       </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
